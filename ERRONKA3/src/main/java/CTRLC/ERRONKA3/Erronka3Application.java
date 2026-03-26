a469eb5234bea595876c0d09ddd3cbdba83d38e0
web25iuribe-commits <web25iuribe@imaltuna.com>
2026-03-26 10:27:24 +0100

    Spring Boot

---
+++ ERRONKA3/src/main/java/CTRLC/ERRONKA3/Erronka3Application.java
@@ -0,0 +1,13 @@
+package CTRLC.ERRONKA3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erronka3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Erronka3Application.class, args);
+	}
+
+}
---
+++ ERRONKA3/src/test/java/CTRLC/ERRONKA3/Erronka3ApplicationTests.java
@@ -0,0 +1,13 @@
+package CTRLC.ERRONKA3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Erronka3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
